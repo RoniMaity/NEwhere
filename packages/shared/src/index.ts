7b814a57be9a92719a237838bfaf45dc79c08625
RoniMaity <maityroni1425@gmail.com>
2026-03-06 00:38:39 +0530

    chore: clean slate, keep only docs

--- packages/shared/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './types/session';
-export * from './types/webrtc';
-
-// We can also add shared constants here later!
-export const CONSTANTS = {
-  SIGNALING_PORT: 8080,
-  WS_PING_INTERVAL: 30000,
-};
